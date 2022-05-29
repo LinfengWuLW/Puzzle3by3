9e201463686ee3e392302613dafc835386618b19
LinfengWuLW <54160011+LinfengWuLW@users.noreply.github.com>
2022-05-29 12:12:00 -0700

    Add files via upload

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+	public Rigidbody2D rb;
+	Vector3 previousPostion;
+	Vector3 mousePosWorld2D;
+	Vector3 velocity;
+	float speed=2000;
+	
+	private void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	public void OnMouseDrag()
+	{
+		previousPostion = rb.position;
+		Vector3 mousePoint = Input.mousePosition;
+		Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(mousePoint);
+		mousePosWorld2D = new Vector3(mousePosWorld.x, mousePosWorld.y, 0);
+		velocity = (mousePosWorld2D - previousPostion) * Time.deltaTime*speed;
+	}
+	void OnMouseUp()
+    {
+		velocity = Vector3.zero;
+    }
+
+	void FixedUpdate()
+    {
+		rb.velocity = velocity;
+	}
+}
